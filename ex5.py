3333cb90677c58120492ef3a9d90e06b9eb7775c
Adrianteri <adriateri@gmail.com>
2015-10-30 08:41:03 +0300

    new file:   ex1.py 	new file:   ex1.py~ 	new file:   ex10.py 	new file:   ex11.py 	new file:   ex12.py 	new file:   ex13.py 	new file:   ex14.py 	new file:   ex15.py 	new file:   ex15_sample.txt 	new file:   ex16.py 	new file:   ex16_sample.txt 	new file:   ex17.py 	new file:   ex18.py 	new file:   ex19.py 	new file:   ex2.py 	new file:   ex3.py 	new file:   ex4.py 	new file:   ex5.py 	new file:   ex6.py 	new file:   ex7.py 	new file:   ex8.py 	new file:   ex9.py 	new file:   from_file.txt 	new file:   to_file.txt

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Adrian  W. Kinyua"
+my_age = 20
+my_height = 69 # height in inches
+my_weight = 58 # weight in kilograms
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d kilograms heavy ." % my_weight 
+print "Actually that's not too heavy ." 
+print "He's got %s eyes and %s hair. " %(my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+#this line is tricky try to get it exactly right
+print "If I add %d, %d and %d I get %d. " %(my_age, my_height, my_weight, my_age + my_height + my_weight)
