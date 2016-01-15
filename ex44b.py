0c7b7d53e0b9853b0a39fb2d6743f401c4d5ebb6
Adrianteri <adriateri@gmail.com>
2016-01-15 04:30:46 -0800

    new file:   ex43.py 	new file:   ex44a.py 	new file:   ex44b.py 	new file:   ex44c.py 	new file:   ex44d.py

---
+++ ex44b.py
@@ -0,0 +1,17 @@
+# override explicitly (inheritance)
+
+class Parent(object):
+
+	def override(self):
+		print "PARENT override()"
+
+class Child(Parent):
+
+    def override(self):
+        print "CHILD override()"
+
+dad = Parent()
+Son = Child()
+
+dad.override()
+son.override()        		
